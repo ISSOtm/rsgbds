bff356ca54b670e9ccad2eb3ff15c14827e40adc
ISSOtm <me@eldred.fr>
2023-10-24 19:36:03 +0200

    Split off lexer into multiple sub-modules
    The original file was way, WAY too long, really.

---
+++ src/asm/language/lexer/char_stream.rs
@@ -0,0 +1,195 @@
+//! The "character stream" functions.
+use super::*;
+
+impl<'fstack> Tokenizer<'fstack, '_, '_, '_, '_> {
+    // Retrieves the "source" string underlying the provided state; if an expansion is active, its
+    // contents are returned, otherwise the state's "root" node is used.
+    // Note that the returned `&str` has already been offset, the `&mut usize` should only be
+    // increased by however many **bytes** end up being consumed.
+    //
+    // Avoid calling this function directly, prefer using [`peek`][Self::peek()].
+    pub(super) fn get_state_source<'ret, 'state: 'ret, 'node: 'ret>(
+        cur_state: &'state mut State,
+        node: &'node Node,
+    ) -> (&'ret str, &'state mut usize) {
+        let mut skip = 0;
+        let (source, cur_offset): (&str, _) = 'outer: {
+            for expansion in cur_state.expansions.iter_mut().rev() {
+                if !expansion.has_ended() {
+                    break 'outer (&expansion.source, &mut expansion.offset);
+                }
+                skip = expansion.parent_skip;
+            }
+            (node.as_ref(), &mut cur_state.offset)
+        };
+        (&source[*cur_offset + skip..], cur_offset)
+    }
+
+    fn with_cur_source<T, F: FnOnce(&str, &mut usize) -> T>(&self, f: F) -> T {
+        debug_assert!(
+            !self.expand_macro_args,
+            "Accessing cur source raw while macro args are active!?"
+        );
+        debug_assert!(
+            !self.enable_interpolation,
+            "Accessing cur source raw while interpolation is enabled!?"
+        );
+
+        let mut lexer = self.lexer.borrow_mut();
+        let cur_node = self.cur_node_handle();
+        cur_node.with_node(|node| {
+            let (source, cur_offset) = Self::get_state_source(lexer.cur_state_mut(), node);
+            f(source, cur_offset)
+        })
+    }
+
+    pub(super) fn peek(&mut self) -> Option<char> {
+        let mut lexer = self.lexer.borrow_mut();
+        let cur_node = self.cur_node_handle();
+        cur_node.with_node(|node| {
+            loop {
+                let (source, cur_offset) = Self::get_state_source(lexer.cur_state_mut(), node);
+                let mut chars = source.chars();
+
+                let c = match chars.next() {
+                    // This prevents re-expanding characters that have already been scanned by a previous `peek` without `bump`.
+                    Some(c) if self.macro_arg_scan_distance != 0 => Some(c),
+
+                    Some('\\') if self.expand_macro_args => {
+                        match self.read_putative_backslash_expansion(chars) {
+                            Some((Ok(expansion), trigger_len)) => {
+                                self.macro_arg_scan_distance += trigger_len; // Macro args aren't recursive.
+
+                                // Don't bother doing the expensive work for empty expansions.
+                                if !expansion.is_empty() {
+                                    self.macro_arg_scan_distance += expansion.len();
+                                    Self::begin_expansion(&mut lexer, expansion, trigger_len);
+                                }
+                                continue;
+                            }
+
+                            Some((Err(kind), trigger_len)) => {
+                                let cur_node = self.fstack.cur_node_handle();
+                                let begin = Location {
+                                    storage: cur_node.clone(),
+                                    offset: *cur_offset,
+                                };
+                                let end = Location {
+                                    storage: cur_node,
+                                    offset: *cur_offset + trigger_len,
+                                };
+                                self.reporter.borrow_mut().report_error(
+                                    self.fstack,
+                                    AsmError::new(begin, end, kind).into(),
+                                );
+
+                                // Skip the bad expansion trigger.
+                                *cur_offset += trigger_len;
+                            }
+
+                            None => {}
+                        }
+                        Some('\\') // If it doesn't introduce a macro arg, then just return it.
+                    }
+                    Some('{') if self.enable_interpolation => {
+                        todo!();
+                    }
+                    Some(c) => Some(c),
+                    None => None,
+                };
+                break c;
+            }
+        })
+    }
+
+    /// Do not call this directly, use either [`bump()`][Self::bump()] or [`bump_capture()`][Self::bump_capture()].
+    fn bump_internal(&mut self) -> char {
+        let mut lexer = self.lexer.borrow_mut();
+
+        let mut skip = 0;
+        let (cur_ofs, bumped_char) = loop {
+            if let Some(expansion) = lexer.cur_state_mut().expansions.last_mut() {
+                if expansion.has_ended() {
+                    debug_assert_eq!(skip, 0, "Cannot apply offset to ended expansion!?");
+
+                    skip = expansion.parent_skip;
+                    lexer.cur_state_mut().expansions.pop();
+                    continue; // Retry with the parent.
+                }
+                let offset = expansion.offset;
+                break (
+                    &mut expansion.offset,
+                    expansion.source[offset..].chars().next(),
+                );
+            } else {
+                let offset = &mut lexer.cur_state_mut().offset;
+                let ofs = *offset + skip; // Account for the "trigger" of any expansion that may have just ended.
+                break self
+                    .cur_node_handle()
+                    .with_node(|node| (offset, node.as_ref()[ofs..].chars().next()));
+            };
+        };
+
+        let c = bumped_char.expect("Cannot shift at EOF!?");
+        *cur_ofs += skip + c.len_utf8();
+        c
+    }
+
+    /// Use to "accept" a character while capture is not active.
+    pub(super) fn bump(&mut self) {
+        self.bump_internal();
+
+        #[cfg(debug_assertions)]
+        if let Some(capture) = &self.capture {
+            panic!("Unexpectedly capturing!? (capture = {capture:?})");
+        }
+    }
+
+    /// Use to "accept" a character while capture is active.
+    /// `should_capture` specifies whether the shifted character must be appended to the capture.
+    pub(super) fn bump_capture(&mut self, should_capture: bool) {
+        let c = self.bump_internal();
+
+        let capture = self.capture.as_mut().expect("Unexpectedly not capturing!?");
+        if should_capture {
+            capture.push(c);
+        }
+    }
+
+    pub(super) fn start_capture(&mut self) {
+        debug_assert!(
+            self.capture.is_none(),
+            "Lexer beginning capture without ending previous!?"
+        );
+
+        let lexer = self.lexer.borrow();
+        let cur_state = lexer.cur_state();
+        let start_ofs = cur_state.offset;
+        self.capture = Some(match cur_state.expansions.last() {
+            Some(expansion) => String::new(),
+            None => self
+                .cur_node_handle()
+                .with_node(|node| node.slice(start_ofs..start_ofs)),
+        });
+    }
+
+    pub(super) fn end_capture(&mut self) -> String {
+        self.capture
+            .take()
+            .expect("Lexer ending capture without starting it!?")
+    }
+
+    pub(super) fn location(
+        storage: Option<NodeHandle<'fstack>>,
+        mut offset: usize,
+    ) -> Location<'fstack> {
+        if let Some(node) = &storage {
+            offset += node.with_node(Node::storage_base_ofs);
+        }
+        Location { storage, offset }
+    }
+
+    pub(super) fn cur_loc(&self) -> Location<'fstack> {
+        Self::location(self.fstack.cur_node_handle(), self.cur_root_offset())
+    }
+}
---
+++ src/asm/language/lexer/mod.rs
@@ -0,0 +1,327 @@
+use std::{cell::RefCell, dbg, debug_assert, debug_assert_eq, ops::Deref, rc::Rc};
+
+use crate::{
+    error::Reporter,
+    fstack::{Fstack, Node, NodeHandle},
+    language::{tokens::can_start_ident, Warning},
+    macro_args::MacroArgs,
+    symbols::Symbols,
+};
+
+use super::{
+    tokens::{Keyword, Token, TrieIndex, TrieIter, KEYWORD_TRIE},
+    AsmError, AsmErrorKind,
+};
+
+/// Unlike state in the [`Fstack`], state in the `Lexer` does not persist once the context is exited.
+#[derive(Debug)]
+pub struct Lexer {
+    states: Vec<State>,
+
+    bin_digits: [char; 2],
+    gfx_digits: [char; 4],
+    pub expand_equs: bool,
+    pub mode: Mode,
+}
+
+#[derive(Debug)]
+struct State {
+    /// Offset within the buffer.
+    offset: usize,
+    expansions: Vec<Expansion>,
+}
+
+#[derive(Debug)]
+struct Expansion {
+    source: Rc<String>,
+    offset: usize,
+    /// How many bytes long the text that triggered this expansion is.
+    parent_skip: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Mode {
+    Normal,
+    Raw,
+    CaptureMacroBody,
+    CaptureLoopBody,
+}
+
+impl Lexer {
+    pub fn new() -> Self {
+        Self {
+            states: vec![State::new()],
+            bin_digits: ['0', '1'],
+            gfx_digits: ['0', '1', '2', '3'],
+            expand_equs: true, // Enabled by default.
+            mode: Mode::Normal,
+        }
+    }
+
+    fn cur_state(&self) -> &State {
+        self.states
+            .last()
+            .expect("There should always be at least one lexer state")
+    }
+
+    fn cur_state_mut(&mut self) -> &mut State {
+        self.states
+            .last_mut()
+            .expect("There should always be at least one lexer state")
+    }
+
+    pub fn push_new_state(&mut self) {
+        self.states.push(State::new());
+    }
+
+    pub fn pop_state(&mut self) {
+        self.states.pop();
+    }
+
+    pub fn cur_ofs(&self) -> usize {
+        self.cur_state().offset
+    }
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            offset: 0,
+            expansions: Vec::new(), // This doesn't allocate.
+        }
+    }
+
+    pub fn reset(&mut self) {
+        // TODO: what if the buffer *ends* with an expansion? Is it still on the stack, but "inactive"?
+        debug_assert_eq!(self.expansions.len(), 0); // No expansion should be active when resetting a state.
+        self.offset = 0;
+    }
+}
+
+impl Expansion {
+    fn has_ended(&self) -> bool {
+        debug_assert!(self.offset <= self.source.len());
+        self.offset == self.source.len()
+    }
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct Location<'fstack> {
+    storage: Option<NodeHandle<'fstack>>,
+    offset: usize,
+}
+
+impl Location<'_> {
+    pub(crate) const fn builtin() -> Location<'static> {
+        Location {
+            storage: None,
+            offset: 0,
+        }
+    }
+
+    pub(crate) fn handle(&self) -> Option<&NodeHandle<'_>> {
+        self.storage.as_ref()
+    }
+
+    pub(crate) fn offset(&self) -> usize {
+        self.offset
+    }
+}
+
+impl PartialOrd for Location<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.storage != other.storage {
+            None
+        } else {
+            Some(self.offset.cmp(&other.offset))
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Tokenizer<'fstack, 'lexer, 'macro_args, 'reporter, 'syms> {
+    fstack: &'fstack Fstack,
+    lexer: &'lexer RefCell<Lexer>,
+    macro_args: &'macro_args RefCell<Vec<MacroArgs>>,
+    reporter: &'reporter RefCell<Reporter>,
+    symbols: &'syms RefCell<Symbols<'fstack>>,
+
+    // These are fine here because they are always both false when a new state is pushed.
+    // TODO: are they really necessary? Don't we always know their state? If so, why not simply pass them as args to `peek()`?
+    expand_macro_args: bool,
+    enable_interpolation: bool,
+
+    /// How many characters have already been scanned for macro args.
+    macro_arg_scan_distance: usize,
+
+    /// If `Some()`, shifted characters are "captured" into this string.
+    capture: Option<String>, // Capture shouldn't be active when changing states.
+
+    /// Is the lexer at the beginning of the line?
+    /// (If `true`, the lexer will next generate a [`LookaheadHack`][Token::LookaheadHack] token.)
+    inject_lookahead_hack: bool,
+}
+
+// A couple of properties.
+
+fn is_whitespace(c: char) -> bool {
+    matches!(c, ' ' | '\t')
+}
+
+fn escape_char(c: char, is_macro_arg: bool) -> Option<char> {
+    match c {
+        ',' | '(' | ')' if is_macro_arg => Some(c),
+        '\\' | '"' | '{' | '}' => Some(c),
+        'n' => Some('\n'),
+        'r' => Some('\r'),
+        't' => Some('\t'),
+        _ => None,
+    }
+}
+
+macro_rules! line_cont_start {
+    () => {
+        ' ' | '\r' | '\n'
+    };
+}
+
+impl<'fstack, 'lexer, 'macro_args, 'reporter, 'syms>
+    Tokenizer<'fstack, 'lexer, 'macro_args, 'reporter, 'syms>
+{
+    pub fn new(
+        fstack: &'fstack Fstack,
+        lexer: &'lexer RefCell<Lexer>,
+        macro_args: &'macro_args RefCell<Vec<MacroArgs>>,
+        reporter: &'reporter RefCell<Reporter>,
+        symbols: &'syms RefCell<Symbols<'fstack>>,
+    ) -> Self {
+        Self {
+            fstack,
+            lexer,
+            macro_args,
+            reporter,
+            symbols,
+
+            expand_macro_args: true,    // Enabled by default.
+            enable_interpolation: true, // Enabled by default.
+
+            macro_arg_scan_distance: 0,
+
+            capture: None, // Disabled by default.
+
+            inject_lookahead_hack: false,
+        }
+    }
+}
+
+/// Helper functions.
+impl<'fstack> Tokenizer<'fstack, '_, '_, '_, '_> {
+    fn cur_root_offset(&self) -> usize {
+        self.lexer.borrow().cur_state().offset
+    }
+
+    fn cur_node_handle(&self) -> NodeHandle<'fstack> {
+        self.fstack
+            .cur_node_handle()
+            .expect("Should have an active fstack node")
+    }
+
+    fn with_active_macro_args<T, F: FnMut(&MacroArgs) -> Result<T, AsmErrorKind>>(
+        &self,
+        mut f: F,
+    ) -> Result<T, AsmErrorKind> {
+        match self.macro_args.borrow().last() {
+            None => Err(AsmErrorKind::NoActiveMacro),
+            Some(args) => f(args),
+        }
+    }
+
+    fn try_get_macro_arg(&self, idx: u32) -> Result<Rc<String>, AsmErrorKind> {
+        if idx == 0 {
+            Err(AsmErrorKind::NoMacroArg0)
+        } else {
+            self.with_active_macro_args(|args| match args.get(idx.try_into().unwrap()) {
+                None => Err(AsmErrorKind::NoMacroArg(idx)),
+                Some(arg) => Ok(Rc::clone(arg)),
+            })
+        }
+    }
+
+    fn read_putative_backslash_expansion<It: Iterator<Item = char>>(
+        &self,
+        mut iter: It,
+    ) -> Option<(Result<Rc<String>, AsmErrorKind>, usize)> {
+        Some(match iter.next()? {
+            c @ '0'..='9' => {
+                let idx = c as u32 - '0' as u32; // Because `to_digit` is inconvenient here.
+                (self.try_get_macro_arg(idx), 2)
+            }
+            '#' => (
+                self.with_active_macro_args(|args| Ok(args.make_concat())),
+                2,
+            ),
+            '<' => {
+                todo!();
+            }
+
+            '@' => (todo!(), 2),
+
+            _ => return None,
+        })
+    }
+
+    fn begin_expansion(lexer: &mut Lexer, source: Rc<String>, trigger_len: usize) {
+        lexer.cur_state_mut().expansions.push(Expansion {
+            source,
+            offset: 0,
+            parent_skip: trigger_len,
+        })
+    }
+}
+
+mod char_stream;
+mod mode_capture_body;
+mod mode_normal;
+mod mode_raw;
+mod tokens;
+
+/// The interface used by the parser.
+impl<'fstack> Iterator for Tokenizer<'fstack, '_, '_, '_, '_> {
+    type Item = Result<(Location<'fstack>, Token, Location<'fstack>), AsmError<'fstack>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let cur_node = self.fstack.cur_node_handle()?; // No active node means we reached the end of input.
+        let storage_base_ofs = cur_node.with_node(Node::storage_base_ofs);
+
+        let mode = self.lexer.borrow().mode;
+        let (res, start_offset) = if std::mem::replace(&mut self.inject_lookahead_hack, false) {
+            (Ok(Token::LookaheadHack), self.cur_root_offset())
+        } else {
+            let token = match mode {
+                Mode::Normal => self.next_normal(),
+                Mode::Raw => self.next_raw(),
+                Mode::CaptureMacroBody => self.next_capture_body(Keyword::Endm, 4),
+                Mode::CaptureLoopBody => self.next_capture_body(Keyword::Endr, 4),
+            };
+            match token {
+                None => {
+                    // We have no more tokens in the active "node", but we want to ensure that all
+                    // directives end with a newline.
+                    let loc = Self::location(Some(cur_node.clone()), self.cur_root_offset());
+                    // FIXME: if you have a `INCLUDE` at EOL without a newline, this will pop off its parent node *before* excuting the `INCLUDE`!!
+                    //        This can be fixed by controlling that the INCLUDE is executed before the newline, but that would require either a "lexer hack" injection (likely right after parsing the `INCLUDE`), or a hand-written parser.
+                    self.fstack.handle_end_of_node(&mut self.lexer.borrow_mut());
+                    return Some(Ok((loc.clone(), Token::Newline, loc)));
+                }
+                Some(token) => token,
+            }
+        };
+        let start_location = Self::location(Some(cur_node.clone()), start_offset);
+        // FIXME: this is wrong for macro args followed by a comma and/or right-trimmed.
+        let end_location = Self::location(Some(cur_node.clone()), self.cur_root_offset());
+        Some(match res {
+            Ok(token) => Ok((start_location, token, end_location)),
+            Err(kind) => Err(AsmError::new(start_location, end_location, kind)),
+        })
+    }
+}
