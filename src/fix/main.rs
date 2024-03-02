73b60d6c55b011e47b95c426c260098381914fe6
Oneirical <manchot@videotron.ca>
2024-03-02 18:06:45 -0500

    fix: some borrow checker issues

--- src/fix/main.rs
+++ src/fix/main.rs
@@ -16,10 +16,8 @@ const TRASH_GLOBAL_SUM: u8 = 0x04;
 
 pub struct CLIOptions {
     game_id: Option<&'static str>,
-    game_id_len: u8,
     japanese: bool,
     new_licensee: Option<&'static str>,
-    new_licensee_len: u8,
     old_licensee: Option<u16>,
     cartridge_type: MbcType,
     rom_version: Option<u16>,
@@ -85,7 +83,7 @@ struct Cli {
     validate: bool,
 
     #[clap(help = "Files to process")]
-    files: Option<Vec<String>>,
+    files: Vec<String>,
 }
 
 enum FixSpec {
@@ -174,12 +172,10 @@ logo![
 
 fn main() {
 
-    let cli_options = CLIOptions {
+    let mut cli_options = CLIOptions {
         game_id: None,
-        game_id_len: 0,
         japanese: true,
         new_licensee: None,
-        new_licensee_len: 0,
         old_licensee: None,
         cartridge_type: MbcType::MbcNone,
         rom_version: None,
@@ -211,38 +207,35 @@ fn main() {
         cli_options.fix_spec = fix_spec;
     }
 
-    if let Some(game_id) = cli.game_id {
-        cli_options.game_id = Some(&game_id); // TOCHECK is game_id_len and all the other lens actually getting used? 
+    if let Some(mut game_id) = cli.game_id {
         let mut len = game_id.len();
         if len > 4 {
-            println!("warning: Truncating game ID \"{}\" to 4 chars", game_id);
+            println!("warning: Truncating game ID \"{}\" to 4 chars", &game_id);
             // Truncate game_id to 4 characters if it's longer
-            let truncated_game_id = &game_id[0..4];
-            println!("Truncated game ID: {}", truncated_game_id);
+            game_id = game_id[0..4].to_string();
+            println!("Truncated game ID: {}", &game_id);
             len = 4;
         } else {
-            println!("Game ID: {}", game_id);
+            println!("Game ID: {}", &game_id);
         }
-        cli_options.game_id_len = len as u8;
+        cli_options.game_id = Some(&game_id); 
     }
 
     if cli.non_japanese {
         cli_options.japanese = false;
     }
 
-    if let Some(new_licensee) = cli.new_licensee {
-        cli_options.new_licensee = Some(&new_licensee); // TOCHECK easier to just remove new_licensee_len and pass the truncated one right away?
+    if let Some(mut new_licensee) = cli.new_licensee {
         let mut len = new_licensee.len() as u8;
         if len > 2 {
             println!("warning: Truncating new licensee \"{}\" to 2 chars", new_licensee);
-            // Truncate game_id to 4 characters if it's longer
-            cli_options.new_licensee = Some(&new_licensee[0..2]); // TOCHECK make sure it's actually getting truncated
+            new_licensee = new_licensee[0..2].to_string();
             println!("Truncated new licencee: {}", &new_licensee[0..2]);
             len = 2
         } else {
             println!("New licensee: {}", new_licensee);
         }
-        cli_options.new_licensee_len = len;    
+        cli_options.new_licensee = Some(&new_licensee);
     }
 
     if let Some(old_licensee) = cli.old_licensee {
@@ -303,7 +296,7 @@ fn main() {
     if cli.color_only || cli.color_compatible {
         cli_options.model = if cli.color_compatible { Model::BOTH } else { Model::CGB };
         if cli_options.title_len > 15 {
-            if let Some(title) = cli_options.title {
+            if let Some(mut title) = cli_options.title {
                 title = &title[0..15];
                 eprintln!("warning: Truncating title \"{}\" to 15 chars", title);
                 cli_options.title = Some(title);
@@ -320,7 +313,7 @@ fn main() {
         //TODO: ???
     }
 
-    if let Some(files) = cli.files {
+    if !cli.files.is_empty() {
         // TODO actually do stuff
     }
 
@@ -388,7 +381,7 @@ enum MbcType {
     MbcBadRange,
 }
 
-fn get_mbc_type_code(mbc_type: MbcType) -> u8 {
+fn get_mbc_type_code(mbc_type: &MbcType) -> u8 {
     match mbc_type {
         MbcType::Rom => 0x00,
         MbcType::RomRam => 0x08,
@@ -434,7 +427,7 @@ fn get_mbc_type_code(mbc_type: MbcType) -> u8 {
         MbcType::Tpp1BatteryTimerRumble => 0x10D,
         MbcType::Tpp1BatteryTimerMultiRumble => 0x10E,
         MbcType::Tpp1BatteryTimerMultiRumbleRumble => 0x10F,
-        MbcType::MbcNone => 0x00, // Assuming this maps to 0x00, adjust as needed
+        _ => 0x00, // Assuming this maps to 0x00, adjust as needed
     }
 }
 
@@ -600,7 +593,6 @@ enum ParseError {
     Bad,
     BadRange,
     Help,
-    // Other error types?
 }
 
 impl FromStr for MbcType {
@@ -619,8 +611,6 @@ impl FromStr for MbcType {
             }
             return Ok(get_mbc_enum(mbc));
         }
-
-        // More parsing of other cases?
 
         Err(ParseError::Bad)
     }
@@ -658,39 +648,26 @@ fn read_bytes<R: Read>(fd: &mut R, buf: &mut [u8]) -> io::Result<usize> {
     Ok(total)
 }
 
-fn write_bytes<W: Write>(fd: &mut W, buf: &[u8]) -> io::Result<usize> {
-    let mut total = 0;
-    while !buf.is_empty() {
-        match fd.write(buf) {
-            Ok(0) => break, // EOF
-            Ok(n) => {
-                total += n;
-                buf = &buf[n..];
-            },
-            Err(e) if e.kind() == io::ErrorKind::Interrupted => continue,
-            Err(e) => return Err(e),
-        }
+fn overwrite_byte(rom0: &mut [u8], addr: u16, fixed_byte: u8, area_name: &str, overwrite_rom: bool) {
+    let orig_byte = rom0[addr as usize];
+
+    if !overwrite_rom && orig_byte != 0 && orig_byte != fixed_byte {
+        writeln!(std::io::stderr(), "warning: Overwrote a non-zero byte in the {}", area_name).unwrap();
     }
-    Ok(total)
+
+    rom0[addr as usize] = fixed_byte;
 }
 
-fn overwrite_byte<W: Write>(fd: &mut W, addr: u16, fixed_byte: u8, area_name: &str) -> io::Result<()> { // TOCHECK: So, is the length of title, etc, all useless since as_bytes suffices?
-    if addr >= 0x8000 {
-        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Address out of bounds"));
-    }
-    fd.write_all(&[fixed_byte])?;
-    println!("warning: Overwrote a byte in the {}", area_name);
-    Ok(())
-}
-
-fn overwrite_bytes(rom0: &mut [u8], start_addr: u16, fixed: &[u8], area_name: &str) -> io::Result<()> {
+fn overwrite_bytes(rom0: &mut [u8], start_addr: u16, fixed: &[u8], area_name: &str, overwrite_rom: bool) -> io::Result<()> {
     if start_addr as usize + fixed.len() > rom0.len() {
         return Err(io::Error::new(io::ErrorKind::InvalidInput, "Address or size out of bounds"));
     }
-    for (i, &byte) in fixed.iter().enumerate() {
-        if rom0[i + start_addr as usize] != 0 && rom0[i + start_addr as usize] != byte {
-            println!("warning: Overwrote a non-zero byte in the {}", area_name);
-            break;
+    if !overwrite_rom {
+        for (i, &byte) in fixed.iter().enumerate() {
+            if rom0[i + start_addr as usize] != 0 && rom0[i + start_addr as usize] != byte {
+                println!("warning: Overwrote a non-zero byte in the {}", area_name);
+                break;
+            }
         }
     }
     rom0[start_addr as usize..start_addr as usize + fixed.len()].copy_from_slice(fixed);
@@ -710,7 +687,7 @@ fn process_file(input: &mut File, output: &mut File, name: &str, file_size: u64,
         Ok(corr_len) => corr_len,
         Err(e) => { eprintln!("Invalid file input."); 0 }// TOCHECK: Crash instead of default 0 value
     };
-    let header_size = if get_mbc_type_code(options.cartridge_type) & 0xFF00 == get_mbc_type_code(MbcType::Tpp1) { 0x154 } else { 0x150 };
+    let header_size = if get_mbc_type_code(&options.cartridge_type) & 0xFF00 == get_mbc_type_code(&MbcType::Tpp1) { 0x154 } else { 0x150 };
 
     if rom0_len < header_size as usize {
         eprintln!("FATAL: \"{}\" too short, expected at least {} bytes, got only {}",
@@ -721,87 +698,88 @@ fn process_file(input: &mut File, output: &mut File, name: &str, file_size: u64,
     for fix_spec in options.fix_spec {
         if matches!(fix_spec, FixSpec::FixLogo) || matches!(fix_spec, FixSpec::TrashLogo) {
             match fix_spec {
-                FixSpec::FixLogo => overwrite_bytes(&mut rom0, 0x0104, &NINTENDO_LOGO, "Nintendo logo"),
-                FixSpec::TrashLogo => overwrite_bytes(&mut rom0, 0x0104, &TRASH_LOGO, "Nintendo logo"),
+                FixSpec::FixLogo => overwrite_bytes(&mut rom0, 0x0104, &NINTENDO_LOGO, "Nintendo logo", options.overwrite_rom),
+                FixSpec::TrashLogo => overwrite_bytes(&mut rom0, 0x0104, &TRASH_LOGO, "Nintendo logo", options.overwrite_rom),
                 _ => Ok(()),
             };
         };
     }
 
     if let Some(title) = options.title {
-        overwrite_bytes(&mut rom0, 0x134, title.as_bytes(), "title");
+        overwrite_bytes(&mut rom0[..], 0x134, title.as_bytes(), "title", options.overwrite_rom);
     }
 
     if let Some(game_id) = options.game_id {
-        overwrite_bytes(&mut rom0, 0x13F, game_id.as_bytes(), "manufacturer code");
+        overwrite_bytes(&mut rom0[..], 0x13F, game_id.as_bytes(), "manufacturer code", options.overwrite_rom);
     }
 
     if !matches!(options.model, Model::DMG) {
         match options.model {
-            Model::BOTH => overwrite_byte(&mut rom0, 0x143, 0x80, "CGB flag"),
-            Model::CGB => overwrite_byte(&mut rom0, 0x143, 0xC0, "CGB flag"),
-            _ => Ok(()),
+            Model::BOTH => overwrite_byte(&mut rom0, 0x143, 0x80, "CGB flag", options.overwrite_rom),
+            Model::CGB => overwrite_byte(&mut rom0, 0x143, 0xC0, "CGB flag", options.overwrite_rom),
+            _ => (),
         };
     };
 
     if let Some(new_licensee) = options.new_licensee {
-        overwrite_bytes(&mut rom0[..], 0x144, new_licensee.as_bytes(), "new licensee code");
+        overwrite_bytes(&mut rom0[..], 0x144, new_licensee.as_bytes(), "new licensee code", options.overwrite_rom);
     }
 
     if options.sgb {
-        overwrite_byte(&mut rom0[..], 0x146, 0x03, "SGB flag");
+        overwrite_byte(&mut rom0[..], 0x146, 0x03, "SGB flag", options.overwrite_rom);
     }
 
     let cartridge_type = options.cartridge_type;
     let ram_size = options.ram_size;
 
-    if get_mbc_type_code(cartridge_type) < get_mbc_type_code(MbcType::MbcNone) {
+    if get_mbc_type_code(&cartridge_type) < get_mbc_type_code(&MbcType::MbcNone) {
         let byte = cartridge_type as u8;
 
-        if (get_mbc_type_code(cartridge_type) & get_mbc_type_code(MbcType::Tpp1)) == get_mbc_type_code(MbcType::Tpp1) {
+        if (get_mbc_type_code(&cartridge_type) & get_mbc_type_code(&MbcType::Tpp1)) == get_mbc_type_code(&MbcType::Tpp1) {
             // Cartridge type isn't directly actionable, translate it
             let byte = 0xBC;
             // The other TPP1 identification bytes will be written below
         }
-        overwrite_byte(&mut rom0[..], 0x147, byte, "cartridge type");
+        overwrite_byte(&mut rom0[..], 0x147, byte, "cartridge type", options.overwrite_rom);
     }
 
     // ROM size will be written last, after evaluating the file's size
 
-    if (get_mbc_type_code(cartridge_type) & get_mbc_type_code(MbcType::Tpp1)) == get_mbc_type_code(MbcType::Tpp1) {
+    if (get_mbc_type_code(&cartridge_type) & get_mbc_type_code(&MbcType::Tpp1)) == get_mbc_type_code(&MbcType::Tpp1) {
         let tpp1_code = vec![0xC1, 0x65];
         let tpp1_rev = vec![0xC1, 0x65]; // TODO WARNING PLACEHOLDER NOT ACTUAL VALUES, PICK UP FROM OPTIONS INSTEAD?
 
         // TODO: I don't understand this part. Tpp1_rev comes from tryReadSlice and I still don't understand very well what it does.
-        overwrite_bytes(&mut rom0[..], 0x149, &tpp1_code, "TPP1 identification code");
+        overwrite_bytes(&mut rom0[..], 0x149, &tpp1_code, "TPP1 identification code", options.overwrite_rom);
 
-        overwrite_bytes(&mut rom0[..], 0x150, &tpp1_rev, "TPP1 revision number");
+        overwrite_bytes(&mut rom0[..], 0x150, &tpp1_rev, "TPP1 revision number", options.overwrite_rom);
 
         if let Some(ram_size) = ram_size {
-            overwrite_byte(&mut rom0, 0x152, ram_size, "RAM size");
+            overwrite_byte(&mut rom0[..], 0x152, ram_size as u8, "RAM size", options.overwrite_rom);
         }
 
-        overwrite_byte(&mut rom0, 0x153, (get_mbc_type_code(cartridge_type) & 0xFF) as u8, "TPP1 feature flags");
+        overwrite_byte(&mut rom0[..], 0x153, (get_mbc_type_code(&cartridge_type) & 0xFF) as u8, "TPP1 feature flags", options.overwrite_rom);
     } else {
         // Regular mappers
 
         if let Some(ram_size) = ram_size {
-            overwrite_byte(&mut rom0, 0x149, ram_size, "RAM size");
+            overwrite_byte(&mut rom0[..], 0x149, ram_size as u8, "RAM size", options.overwrite_rom);
         }
 
         if !options.japanese {
-            overwrite_byte(&mut rom0, 0x14A, 0x01, "destination code");
+            overwrite_byte(&mut rom0[..], 0x14A, 0x01, "destination code", options.overwrite_rom);
         }
     }
 
     if let Some(old_licensee) = options.old_licensee {
-        overwrite_byte(&mut rom0, 0x14B, old_licensee as u8, "old licensee code");
+        overwrite_byte(&mut rom0[..], 0x14B, old_licensee as u8, "old licensee code", options.overwrite_rom);
     } else if options.sgb && rom0[0x14B] != 0x33 {
         eprintln!("warning: SGB compatibility enabled, but old licensee was 0x{:02x}, not 0x33", rom0[0x14B]);
     }
     
     if let Some(rom_version) = options.rom_version {
-        overwrite_byte(&mut rom0, 0x14C, rom_version as u8, "mask ROM version number");
+        overwrite_byte(&mut rom0[..], 0x14C, rom_version as u8, "mask ROM version number", options.overwrite_rom);
     }
     
+    Ok(())
 }
