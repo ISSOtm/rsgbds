3e266b4b66a0d5ad2cbd79f085a0ffa90322a4df
Oneirical <manchot@videotron.ca>
2024-03-01 22:25:55 -0500

    add more byte overwrites and remove UNSPECIFIED

--- src/fix/main.rs
+++ src/fix/main.rs
@@ -2,8 +2,8 @@ use std::str::FromStr;
 use clap::{Arg, App, SubCommand, Parser};
 use std::io::{self, Read, Write, Seek, SeekFrom};
 use std::os::unix::io::AsRawFd;
+use std::mem::size_of_val;
 
-const UNSPECIFIED: u32 = 0x200; // Should not be in byte range
 const BANK_SIZE: u32 = 0x4000;
 const OPTSTRING: &str = "Ccf:i:jk:l:m:n:Op:r:st:Vv";
 const FIX_LOGO: u8 = 0x80;
@@ -12,7 +12,6 @@ const FIX_HEADER_SUM: u8 = 0x20;
 const TRASH_HEADER_SUM: u8 = 0x10;
 const FIX_GLOBAL_SUM: u8 = 0x08;
 const TRASH_GLOBAL_SUM: u8 = 0x04;
-const UNSPECIFIED: u16 = 0;
 
 macro_rules! logo {
 	(
@@ -58,12 +57,12 @@ pub struct CLIOptions {
     japanese: bool,
     new_licensee: Option<&'static str>,
     new_licensee_len: u8,
-    old_licensee: u16,
+    old_licensee: Option<u16>,
     cartridge_type: MbcType,
-    rom_version: u16,
+    rom_version: Option<u16>,
     overwrite_rom: bool,
-    pad_value: u16,
-    ram_size: u16,
+    pad_value: Option<u16>,
+    ram_size: Option<u16>,
     sgb: bool,
     model: Model,
     title: Option<&'static str>,
@@ -125,6 +124,15 @@ struct Cli {
     files: Vec<String>,
 }
 
+enum FixSpec {
+    FixLogo,
+    TrashLogo,
+    FixHeaderSum,
+    TrashHeaderSum,
+    FixGlobalSum,
+    TrashGlobalSum,
+}
+
 fn main() {
 
     let cli_options = CLIOptions {
@@ -133,14 +141,15 @@ fn main() {
         japanese: true,
         new_licensee: None,
         new_licensee_len: 0,
-        old_licensee: UNSPECIFIED,
+        old_licensee: None,
         cartridge_type: MbcType::MBC_NONE,
-        rom_version: UNSPECIFIED,
+        rom_version: None,
         overwrite_rom: false,
-        pad_value: UNSPECIFIED,
-        ram_size: UNSPECIFIED,
+        pad_value: None,
+        ram_size: None,
         sgb: false,
         model: Model::DMG,
+        fix_spec: FixSpec,
         title: None,
         title_len: 0,
     };
@@ -156,19 +165,19 @@ fn main() {
     }
 
     if let Some(fix_spec) = cli.fix_spec {
-        // TODO call override_spec instead of setting a string
         let fix_spec = fix_spec.chars().map(|c| match c {
-            'l' => "FIX_LOGO",
-            'L' => "TRASH_LOGO",
-            'h' => "FIX_HEADER_SUM",
-            'H' => "TRASH_HEADER_SUM",
-            'g' => "FIX_GLOBAL_SUM",
-            'G' => "TRASH_GLOBAL_SUM",
+            'l' => FixSpec::FixLogo,
+            'L' => FixSpec::TrashLogo,
+            'h' => FixSpec::FixHeaderSum,
+            'H' => FixSpec::TrashHeaderSum,
+            'g' => FixSpec::FixGlobalSum,
+            'G' => FixSpec::TrashGlobalSum,
             _ => {
                 eprintln!("warning: Ignoring '{}' in fix spec", c);
-                ""
+                FixSpec::FixLogo // TOCHECK: Default value - is this bad practice? Should the program crash?
             }
         }).collect::<Vec<_>>();
+        cli_options.fix_spec = fix_spec;
         println!("Fix spec set to: {:?}", fix_spec);    }
 
     if let Some(game_id) = cli.game_id {
@@ -565,7 +574,7 @@ fn write_bytes<W: Write>(fd: &mut W, buf: &[u8]) -> io::Result<usize> {
     Ok(total)
 }
 
-fn overwrite_byte<W: Write>(fd: &mut W, addr: u16, fixed_byte: u8, area_name: &str) -> io::Result<()> {
+fn overwrite_byte<W: Write>(fd: &mut W, addr: u16, fixed_byte: u8, area_name: &str) -> io::Result<()> { // TOCHECK: So, is the length of title, etc, all useless since as_bytes suffices?
     if addr >= 0x8000 {
         return Err(io::Error::new(io::ErrorKind::InvalidInput, "Address out of bounds"));
     }
@@ -588,7 +597,7 @@ fn overwrite_bytes(rom0: &mut [u8], start_addr: u16, fixed: &[u8], area_name: &s
     Ok(())
 }
 
-fn process_file(input: &mut File, output: &mut File, name: &str, file_size: u64) -> io::Result<()> {
+fn process_file(input: &mut File, output: &mut File, name: &str, file_size: u64, options: CLIOptions) -> io::Result<()> {
     // Check if the file is seekable
     if input.as_raw_fd() == output.as_raw_fd() {
         assert!(file_size != 0);
@@ -599,7 +608,7 @@ fn process_file(input: &mut File, output: &mut File, name: &str, file_size: u64)
     let mut rom0 = [0u8; BANK_SIZE];
     let rom0_len = read_bytes(input, rom0);
 
-    let header_size = if CARTRIDGE_TYPE & 0xFF00 == TPP1 { 0x154 } else { 0x150 }; // TODO cart_type will be in options struct
+    let header_size = if options.cartridge_type & 0xFF00 == MbcType::Tpp1 { 0x154 } else { 0x150 };
 
     if rom0_len == -1 {
         report("FATAL: Failed to read \"{}\"'s header", name);
@@ -609,4 +618,90 @@ fn process_file(input: &mut File, output: &mut File, name: &str, file_size: u64)
                  name, header_size, rom0_len);
         return Err(io::Error::new(io::ErrorKind::Other, "File too short"));
     }
+
+    if options.fix_spec & (FixSpec::FixLogo | FixSpec::TrashLogo) {
+        if options.fix_spec & FixSpec::FixLogo {
+            overwrite_bytes(&mut rom0, 0x0104, NINTENDO_LOGO, size_of_val(NINTENDO_LOGO), "Nintendo logo");
+        }
+        else {
+            overwrite_bytes(&mut rom0, 0x0104, TRASH_LOGO, size_of_val(TRASH_LOGO), "Nintendo logo");
+        }
+    }
+
+    if let Some(title) = options.title {
+        overwrite_bytes(&mut rom0, 0x134, title.as_bytes(), "title");
+    }
+
+    if let Some(game_id) = options.game_id {
+        overwrite_bytes(&mut rom0, 0x13F, game_id.as_bytes(), "manufacturer code");
+    }
+
+    if !matches!(options.model, Model::DMG) {
+        match options.model {
+            Model::BOTH => overwrite_byte(&mut rom0, 0x143, 0x80, "CGB flag"),
+            Model::CGB => overwrite_byte(&mut rom0, 0x143, 0xC0, "CGB flag"),
+
+        }
+    }
+
+    if let Some(new_licensee) = options.new_licensee {
+        overwrite_byte(&mut rom0, 0x144, new_licensee.as_bytes(), "new licensee code"),
+    }
+
+    if sgb {
+        overwriteByte(&mut rom0, 0x146, 0x03, "SGB flag");
+    }
+
+    let cartridge_type = options.cartridge_type;
+    let ram_size = options.ram_size;
+
+    if cartridge_type < MbcType::None {
+        let byte = cartridge_type as u8;
+
+        if (cartridge_type & MbcType::Tpp1) == MbcType::Tpp1 {
+            // Cartridge type isn't directly actionable, translate it
+            let byte = 0xBC;
+            // The other TPP1 identification bytes will be written below
+        }
+        overwrite_byte(&mut rom0, 0x147, byte, "cartridge type");
+    }
+
+    // ROM size will be written last, after evaluating the file's size
+
+    if (cartridge_type & MbcType::Tpp1) == MbcType::Tpp1 {
+        let tpp1_code = vec![0xC1, 0x65];
+        let tpp1_rev = vec![0xC1, 0x65]; // TODO WARNING PLACEHOLDER NOT ACTUAL VALUES, PICK UP FROM OPTIONS INSTEAD?
+
+        // TODO: I don't understand this part. Tpp1_rev comes from tryReadSlice and I still don't understand very well what it does.
+        overwrite_bytes(&mut rom0, 0x149, &tpp1_code, "TPP1 identification code");
+
+        overwrite_bytes(&mut rom0, 0x150, &tpp1_rev, "TPP1 revision number");
+
+        if let Some(ram_size) = ram_size {
+            overwrite_byte(&mut rom0, 0x152, ram_size, "RAM size");
+        }
+
+        overwrite_byte(&mut rom0, 0x153, (cartridge_type & 0xFF) as u8, "TPP1 feature flags");
+    } else {
+        // Regular mappers
+
+        if let Some(ram_size) = ram_size {
+            overwrite_byte(&mut rom0, 0x149, ram_size, "RAM size");
+        }
+
+        if !options.japanese {
+            overwrite_byte(&mut rom0, 0x14A, 0x01, "destination code");
+        }
+    }
+
+    if let Some(old_licensee) = options.old_licensee {
+        overwrite_byte(&mut rom0, 0x14B, old_licensee, "old licensee code");
+    } else if sgb && rom0[0x14B] != 0x33 {
+        eprintln!("warning: SGB compatibility enabled, but old licensee was 0x{:02x}, not 0x33", rom0[0x14B]);
+    }
+    
+    if let Some(rom_version) = options.rom_version {
+        overwrite_byte(&mut rom0, 0x14C, rom_version, "mask ROM version number");
+    }
+    
 }
