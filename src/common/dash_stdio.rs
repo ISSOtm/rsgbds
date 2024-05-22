a1055dc8428d776675b8902a738a9f226f1b03a2
ISSOtm <me@eldred.fr>
2024-05-22 16:01:29 +0200

    Implement `BufRead` for `Input`
    This inflates its size a little, but that should be acceptable

--- src/common/dash_stdio.rs
+++ src/common/dash_stdio.rs
@@ -1,12 +1,12 @@
 use std::{
     fs::File,
-    io::{Read, Write},
+    io::{BufRead, BufReader, BufWriter, Read, StdinLock, StdoutLock, Write},
     path::Path,
 };
 
-pub enum Input {
-    File(File),
-    Stdin,
+pub enum Input<'stdin> {
+    File(BufReader<File>),
+    Stdin(StdinLock<'stdin>),
 }
 
 pub enum Output {
@@ -20,12 +20,12 @@ pub fn read<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<u8>> {
     Ok(buf)
 }
 
-impl Input {
+impl Input<'_> {
     pub fn new<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
         Ok(if path.as_ref() == Path::new("-") {
-            Self::Stdin
+            Self::Stdin(std::io::stdin().lock())
         } else {
-            Self::File(File::open(path)?)
+            Self::File(BufReader::new(File::open(path)?))
         })
     }
 }
@@ -33,68 +33,66 @@ impl Input {
 impl Output {
     pub fn new<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
         Ok(if path.as_ref() == Path::new("-") {
-            Self::Stdout
+            Self::Stdout(std::io::stdout().lock())
         } else {
-            Self::File(File::create(path)?)
+            Self::File(BufWriter::new(File::create(path)?))
         })
     }
 }
 
 macro_rules! forward {
-    (const $Stdio:ident = $io_func:expr; $(fn $name:ident $params:tt -> $ret:ty;)*) => {$(
-        forward_one!($Stdio, $io_func, $name, $params, $ret);
+    ($Stdio:pat => $io:expr, $(fn $name:ident $params:tt $( -> $ret:ty )?;)*) => {$(
+        forward_one!($Stdio, $io, $name, $params, $($ret)?);
     )*};
 }
 
 macro_rules! forward_one {
-    ($Stdio:ident, $io_func:expr, $name:ident, (&self $(
+    ($Stdio:pat, $io:expr, $name:ident, (&self $(
         , $param_name:ident: $param_ty:ty
-    )*), $ret:ty) => {
-        fn $name(&self $(, $param_name: $param_ty)*) -> $ret {
+    )*), $($ret:ty)?) => {
+        fn $name(&self $(, $param_name: $param_ty)*) $( -> $ret )? {
             match self {
-                Self::$Stdio => $io_func.$name($($param_name),*),
+                Self::$Stdio => $io.$name($($param_name),*),
                 Self::File(file) => file.$name($($param_name),*),
             }
         }
     };
-    ($Stdio:ident, $io_func:expr, $name:ident, (&mut self $(
+    ($Stdio:pat, $io:expr, $name:ident, (&mut self $(
         , $param_name:ident: $param_ty:ty
-    )*), $ret:ty) => {
-        fn $name(&mut self $(, $param_name: $param_ty)*) -> $ret {
+    )*), $($ret:ty)?) => {
+        fn $name(&mut self $(, $param_name: $param_ty)*) $( -> $ret )? {
             match self {
-                Self::$Stdio => $io_func.$name($($param_name),*),
+                $Stdio => $io.$name($($param_name),*),
                 Self::File(file) => file.$name($($param_name),*),
             }
         }
     };
 }
 
-impl Read for Input {
-    forward! {
-        const Stdin = std::io::stdin();
-
+impl Read for Input<'_> {
+    forward! { Self::Stdin(lock) => lock,
         fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize>;
-
         fn read_vectored(&mut self, bufs: &mut [std::io::IoSliceMut<'_>]) -> std::io::Result<usize>;
-
         fn read_to_end(&mut self, buf: &mut Vec<u8>) -> std::io::Result<usize>;
-
         fn read_to_string(&mut self, buf: &mut String) -> std::io::Result<usize>;
-
         fn read_exact(&mut self, buf: &mut [u8]) -> std::io::Result<()>;
     }
 }
 
+impl BufRead for Input<'_> {
+    forward! { Self::Stdin(lock) => lock,
+        fn fill_buf(&mut self) -> std::io::Result<&[u8]>;
+        fn consume(&mut self, amt: usize);
+        fn read_until(&mut self, byte: u8, buf: &mut Vec<u8>) -> std::io::Result<usize>;
+        fn read_line(&mut self, buf: &mut String) -> std::io::Result<usize>;
+    }
+}
+
 impl Write for Output {
-    forward! {
-        const Stdout = std::io::stdout();
-
+    forward! { Self::Stdout => std::io::stdout(),
         fn write(&mut self, buf: &[u8]) -> std::io::Result<usize>;
-
         fn flush(&mut self) -> std::io::Result<()>;
-
         fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()>;
-
         fn write_fmt(&mut self, fmt: std::fmt::Arguments<'_>) -> std::io::Result<()>;
     }
 }
